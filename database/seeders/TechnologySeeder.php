bc247e6c62a18642a4338976c88d9c7ce0c00100
salvomc <smcrwrk@gmail.com>
2023-09-05 16:17:40 +0200

    [FEAT] created/seeded technology table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -15,5 +17,14 @@ class TechnologySeeder extends Seeder
     public function run()
     {
         //
+        $techs = ['Html', 'Laravel', 'Scss', 'Symphony', 'JavaScript', 'VueJs'];
+
+        foreach ($techs as $tech) {
+            $new_tech = New Technology();
+            $new_tech->name = $tech;
+            $new_tech->slug = Str::slug( $tech, '-');
+
+            $new_tech->save();
+        }
     }
 }
